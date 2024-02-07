6c0e830decfac51a6855d567d3867607b16ba21f
이기문 <146897750+rlans2067@users.noreply.github.com>
2024-02-07 22:21:01 +0900

    [Silver I] Title: NM과 K (1), Time: 416 ms, Memory: 111584 KB -BaekjoonHub

---
+++ 백준/Silver/18290. NM과 K （1）/NM과 K （1）.py
@@ -0,0 +1,29 @@
+n, m, k = map(int,input().split())
+a = [list(map(int,input().split())) for _ in range(n)]
+c = [[False]*m for _ in range(n)]
+ans = -2147483647
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+def go(prev, cnt, s):
+  if cnt == k:
+    global ans
+    if ans < s:
+      ans = s
+    return
+  for j in range(prev+1, n*m):
+    x = j//m
+    y = j%m
+    if c[x][y]:
+      continue
+    ok = True
+    for i in range(4):
+      nx, ny = x+dx[i], y+dy[i]
+      if 0 <= nx < n and 0 <= ny < m:
+        if c[nx][ny]:
+          ok = False
+    if ok:
+      c[x][y] = True
+      go(x*m+y, cnt+1, s+a[x][y])
+      c[x][y] = False
+go(-1, 0, 0)
+print(ans)
